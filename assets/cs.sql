7d7981b618bfc3cc60560d0dcd74673fc9ec02d8
herrnorbert <nkobli@yahoo.com>
2018-01-19 13:38:38 +0100

    feat(assets): add sql queries

---
+++ assets/cs.sql
@@ -0,0 +1,18 @@
+INSERT INTO users (name)
+VALUES
+('Egg'),
+('Szilvi'),
+('Kevi'),
+('Levi'),
+('Norbi'),
+('Gabor'),
+('Gyuri'),
+('Bogi');
+GO
+
+INSERT INTO tickets (reporter, manufacturer, serialnumber, description, date)
+VALUES
+('Egg', 'dell', 123456789, 'screen pixel error', '2018.01.16'),
+('Szilvi', 'dell', 987654321, 'touchpad not working', '2018.01.17'),
+('Kevi', 'lenovo', 462587618, 'wont boot', '2018.01.19');
+GO
