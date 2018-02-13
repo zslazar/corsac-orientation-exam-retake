1efd6160453c3d2f833fa63664e8e9d72cff0516
zslazar <zs.lazar@gmail.com>
2018-02-13 09:50:31 +0100

    feat(ErrorReporter) base structure

---
+++ ErrorReporter/ErrorReporter/Entities/ErrorContext.cs
@@ -0,0 +1,18 @@
+﻿using ErrorReporter.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorReporter.Entities
+{
+    public class ErrorContext : DbContext
+    {
+        public ErrorContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
