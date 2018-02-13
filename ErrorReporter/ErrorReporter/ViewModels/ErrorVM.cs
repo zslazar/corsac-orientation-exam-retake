7277bcbf5e59b72298e903495fca3635a85ba43c
zslazar <zs.lazar@gmail.com>
2018-02-13 10:22:58 +0100

    feat(ErrorReporter) viewmodel added

---
+++ ErrorReporter/ErrorReporter/ViewModels/ErrorVM.cs
@@ -0,0 +1,16 @@
+﻿using ErrorReporter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorReporter.ViewModels
+{
+    public class ErrorVM
+    {
+        public List<User> Users { get; set; }
+        public Ticket Manufacturer { get; set; }
+        public Ticket SerialNumber { get; set; }
+        public Ticket Description { get; set; }
+    }
+}
