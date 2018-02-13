294804fdcadc8df752d2e559be3b9af0f9b79a36
zslazar <zs.lazar@gmail.com>
2018-02-13 13:06:28 +0100

    feat(ErrorReporter) answers added

--- ErrorReporter/ErrorReporter/ViewModels/ErrorVM.cs
+++ ErrorReporter/ErrorReporter/ViewModels/ErrorVM.cs
@@ -6,10 +6,6 @@ namespace ErrorReporter.ViewModels
     public class ErrorVM
     {
         public List<User> Users { get; set; }
-        public List<Ticket> Tickets { get; set; } 
-
-        //public Ticket Manufacturer { get; set; }
-        //public Ticket SerialNumber { get; set; }
-        //public Ticket Description { get; set; }
+        public List<Ticket> Tickets { get; set; }         
     }
 }
