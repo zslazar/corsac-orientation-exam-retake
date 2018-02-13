c390324cc365011ab79640a2aa112b0ddf09521e
zslazar <zs.lazar@gmail.com>
2018-02-13 09:38:44 +0100

    feat(ErrorReporter) project setup, model added

---
+++ ErrorReporter/ErrorReporter/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorReporter.Models
+{
+    public class Ticket
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public double SerialNumber { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; } = DateTime.Now.ToString("dd/MM/yyyy");
+    }
+}
